5c2c5124c6a8d2db45827909fd8a0da5629f6492
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-18 12:07:32 +0400

    Add Success construct test

--- tests/Test/ShinglesTest.php
+++ tests/Test/ShinglesTest.php
@@ -9,4 +9,10 @@ class ShinglesTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('InvalidArgumentException', '$shinglesCount must be int type');
         new \Shingles\MD5('lol');
     }
+
+    public function testConstructSuccess()
+    {
+        $shingler = new \Shingles\MD5(3);
+        $this->assertInstanceOf('\Shingles\Shingles', $shingler);
+    }
 }
